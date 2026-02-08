c9528d8568c3b89b64d4dc15b3b5cb9aaf85600a
sanghyun6 <sanghyunleeee@gmail.com>
2026-02-07 20:57:51 -0800

    week3_lab

--- lab04/driver.cpp
+++ lab04/driver.cpp
@@ -1,1 +1,71 @@
 // TO DO: Implement the driver main() function to test your code.
+#include "LinkedList.hpp"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    LinkedList<int> list;
+    LinkedList<string> strList;
+
+    list.append(10);
+    list.append(20);
+    cout << list;
+
+    list.replace(1, 50);
+    cout << list;
+
+    try
+    {
+        list.replace(30, 50);
+    }
+    catch (const string &e)
+    {
+        cout << e << endl;
+    }
+
+    cout << list.getElement(1) << endl;
+
+    try
+    {
+        list.getElement(5);
+    }
+    catch (const string &e)
+    {
+        cout << e << endl;
+    }
+
+    list.clear();
+    cout << list;
+
+    strList.append("hi");
+    strList.append("col");
+
+    strList.replace(0, "bye");
+    cout << strList;
+
+    try
+    {
+        strList.replace(3, "ln");
+    }
+    catch (const string &e)
+    {
+        cout << e << endl;
+    }
+
+    cout << strList.getElement(0) << endl;
+
+    try
+    {
+        cout << strList.getElement(8) << endl;
+    }
+    catch (const string &e)
+    {
+        cout << e << endl;
+    }
+
+    strList.clear();
+    cout << strList;
+
+    return 0;
+}
---
+++ lab04/tempCodeRunnerFile.cpp
@@ -0,0 +1,40 @@
+try
+    // {
+    //     list.getElement(5);
+    // }
+    // catch (const string &e)
+    // {
+    //     cout << e << endl;
+    // }
+
+    // list.clear();
+    // cout << list;
+
+    // strList.append("hi");
+    // strList.append("col");
+
+    // strList.replace(0, "bye");
+    // cout << strList;
+
+    // try
+    // {
+    //     strList.replace(3, "ln");
+    // }
+    // catch (const string &e)
+    // {
+    //     cout << e << endl;
+    // }
+
+    // strList.getElement(0);
+
+    // try
+    // {
+    //     strList.getElement(8);
+    // }
+    // catch (const string &e)
+    // {
+    //     cout << e << endl;
+    // }
+
+    // strList.clear();
+    // cout << strList;
